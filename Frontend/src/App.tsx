6e5978ddfee24af27f88c9431e8d56385586e845
A12ise <sorrawit456flookza@gmail.com>
2025-05-07 13:20:54 +0700

    Test commit #3

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React </h1>
       <h1>B6500900</h1>
       <h1>B6525781</h1>
       <div className="card">
